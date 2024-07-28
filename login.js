2ff6afe7d5498e258552ba8709c7fbed728df86d
Dharana Damindu <45477818+dharanadamindu@users.noreply.github.com>
2024-07-28 09:29:09 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,2 +1,2 @@
-const userName = U2FsdGVkX19BORpi2AsLK+6Op4H81V/hgyHHYrdc8l6GUDYJTXaoNcvC3hfEKT2b;
-const Password = U2FsdGVkX1/92ALzY59+R1SxIYXGZk5mxRUaWyUWM9yhMpsm4S6t33osdwprzZ2K;
+const encryptedUsername = U2FsdGVkX19BORpi2AsLK+6Op4H81V/hgyHHYrdc8l6GUDYJTXaoNcvC3hfEKT2b;
+const encryptedPassword = U2FsdGVkX1/92ALzY59+R1SxIYXGZk5mxRUaWyUWM9yhMpsm4S6t33osdwprzZ2K;
